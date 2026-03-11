3b75bdb700e44b75554ed959acf4a51529f0b545
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-11 15:46:16 +0300

    IndexesTable reworked

--- src/Pure.RelationalSchema.Self.Schema/Tables/IndexesTable.cs
+++ src/Pure.RelationalSchema.Self.Schema/Tables/IndexesTable.cs
@@ -3,6 +3,7 @@ using Pure.RelationalSchema.Abstractions.Column;
 using Pure.RelationalSchema.Abstractions.Index;
 using Pure.RelationalSchema.Abstractions.Table;
 using Pure.RelationalSchema.Self.Schema.Columns;
+using Pure.RelationalSchema.Self.Schema.Indexes;
 using String = Pure.Primitives.String.String;
 
 namespace Pure.RelationalSchema.Self.Schema.Tables;
@@ -12,7 +13,7 @@ public sealed record IndexesTable : ITable
     public IString Name => new String("indexes");
 
     public IEnumerable<IColumn> Columns =>
-        [new IsUniqueColumn(), new CompositionHashColumn()];
+        [new UuidColumn(),new IsUniqueColumn(), new CompositionHashColumn()];
 
-    public IEnumerable<IIndex> Indexes => [];
+    public IEnumerable<IIndex> Indexes => [new UuidUniqueIndex()];
 }
