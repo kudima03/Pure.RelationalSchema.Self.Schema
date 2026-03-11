d3b1234c6b6a15a869edcafde80a0ec8373cda80
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-11 15:46:06 +0300

    ForeignKeysTable reworked

--- src/Pure.RelationalSchema.Self.Schema/Tables/ForeignKeysTable.cs
+++ src/Pure.RelationalSchema.Self.Schema/Tables/ForeignKeysTable.cs
@@ -3,6 +3,7 @@ using Pure.RelationalSchema.Abstractions.Column;
 using Pure.RelationalSchema.Abstractions.Index;
 using Pure.RelationalSchema.Abstractions.Table;
 using Pure.RelationalSchema.Self.Schema.Columns;
+using Pure.RelationalSchema.Self.Schema.Indexes;
 using String = Pure.Primitives.String.String;
 
 namespace Pure.RelationalSchema.Self.Schema.Tables;
@@ -12,11 +13,11 @@ public sealed record ForeignKeysTable : ITable
     public IString Name => new String("foreign_keys");
 
     public IEnumerable<IColumn> Columns =>
-        [
+        [new UuidColumn(),
             new ReferencingTableColumn(),
             new ReferencedTableColumn(),
             new CompositionHashColumn(),
         ];
 
-    public IEnumerable<IIndex> Indexes => [];
+    public IEnumerable<IIndex> Indexes => [new UuidUniqueIndex()];
 }
