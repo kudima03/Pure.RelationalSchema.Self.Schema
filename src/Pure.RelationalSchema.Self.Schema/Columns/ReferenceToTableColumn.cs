57043cf5295c4b3fa84b3cb7cb6d9da0d9d380a3
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-11 15:40:54 +0300

    ReferenceToTableColumn reworked

--- src/Pure.RelationalSchema.Self.Schema/Columns/ReferenceToTableColumn.cs
+++ src/Pure.RelationalSchema.Self.Schema/Columns/ReferenceToTableColumn.cs
@@ -8,7 +8,7 @@ namespace Pure.RelationalSchema.Self.Schema.Columns;
 
 public sealed record ReferenceToTableColumn : IColumn
 {
-    public IString Name => new String("table_hash");
+    public IString Name => new String("table_uuid");
 
-    public IColumnType Type => new DeterminedHashColumnType();
+    public IColumnType Type => new UuidColumnType();
 }
