12cdf170424c3d391933b2eb3829446c44a1d3aa
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-11 15:46:38 +0300

    TablesTable reworked

--- src/Pure.RelationalSchema.Self.Schema/Tables/TablesTable.cs
+++ src/Pure.RelationalSchema.Self.Schema/Tables/TablesTable.cs
@@ -3,6 +3,7 @@ using Pure.RelationalSchema.Abstractions.Column;
 using Pure.RelationalSchema.Abstractions.Index;
 using Pure.RelationalSchema.Abstractions.Table;
 using Pure.RelationalSchema.Self.Schema.Columns;
+using Pure.RelationalSchema.Self.Schema.Indexes;
 using String = Pure.Primitives.String.String;
 
 namespace Pure.RelationalSchema.Self.Schema.Tables;
@@ -12,7 +13,7 @@ public sealed record TablesTable : ITable
     public IString Name => new String("tables");
 
     public IEnumerable<IColumn> Columns =>
-        [new NameColumn(), new CompositionHashColumn()];
+        [new UuidColumn(),new NameColumn(), new CompositionHashColumn()];
 
-    public IEnumerable<IIndex> Indexes => [];
+    public IEnumerable<IIndex> Indexes => [new UuidUniqueIndex()];
 }
