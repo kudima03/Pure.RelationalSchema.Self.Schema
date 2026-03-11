b89d1c486f29ed1477d592dc22a63b4a61b34108
Dmitry Kurochkin <kudima03@gmail.com>
2026-03-11 15:45:14 +0300

    TablesToColumnsTableForeignKey reworked

--- src/Pure.RelationalSchema.Self.Schema/ForeignKeys/TablesToColumnsTableForeignKey.cs
+++ src/Pure.RelationalSchema.Self.Schema/ForeignKeys/TablesToColumnsTableForeignKey.cs
@@ -1,7 +1,6 @@
 using Pure.RelationalSchema.Abstractions.Column;
 using Pure.RelationalSchema.Abstractions.ForeignKey;
 using Pure.RelationalSchema.Abstractions.Table;
-using Pure.RelationalSchema.Column;
 using Pure.RelationalSchema.Self.Schema.Columns;
 using Pure.RelationalSchema.Self.Schema.Tables;
 
@@ -15,5 +14,5 @@ public sealed record TablesToColumnsTableForeignKey : IForeignKey
 
     public ITable ReferencedTable => new TablesTable();
 
-    public IEnumerable<IColumn> ReferencedColumns => [new RowDeterminedHashColumn()];
+    public IEnumerable<IColumn> ReferencedColumns => [new UuidColumn()];
 }
